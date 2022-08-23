4e5726a9e743f7bd5bf7825c8d17f325be46fcd5
lisanyny <110669469+lisanyny@users.noreply.github.com>
2022-08-23 01:02:06 +0000

    [start]4-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   def new
     #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.newa
+    @list = List.new
   end
 
   def create
@@ -9,17 +9,20 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     #3.データをデータベースに保存するためのsaveメソッド実行
     list.save
-    #4.トップ画面へリダイレクト
-    redirect_to '/top'
+    #詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
   private
